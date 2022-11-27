cbac318401087d62d3e63cf11215339a730be21a
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-11-27 23:02:20 +0000

    Added hello packet

--- Code/Packets.py
+++ Code/Packets.py
@@ -1,4 +1,4 @@
-typeToNum = {"Message": 0}
+typeToNum = {"Message": 0, "Hello": 1}
 
 class MessagePacket():
 	def __init__(self, dest, payload):
@@ -25,7 +25,26 @@ class MessagePacket():
 		payload = packet[4:]
 		return cls(dest, payload)
 
-numToClass = {0: MessagePacket}
+class HelloPacket():
+	def __init__(self, routerIP):
+		self.type = typeToNum["Hello"]
+		self.routerIP = routerIP
+	
+	def encode(self):
+		# Store type of packet in first byte
+		typeByte = (self.type).to_bytes(1, byteorder='big')
+		encoding = typeByte
+		encoding += str.encode(self.routerIP)
+		# Return packet bytes
+		return encoding
+
+	@classmethod
+	def decode(cls, packet):
+		# Everything after type byte is filename	
+		routerIP = packet[1:].decode()
+		return cls(routerIP)
+
+numToClass = {0: MessagePacket, 1: HelloPacket}
 
 def decodePacket(packet):
 	packetType = packet[0]
