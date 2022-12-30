8760871ab5ad65fdd57487b968c656bb836ef8b1
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-12-30 20:56:14 +0000

    Add addService packet type.

--- Code/Packets.py
+++ Code/Packets.py
@@ -1,4 +1,4 @@
-typeToNum = {"Message": 0, "RequestInfo": 1, "NextHop": 2}
+typeToNum = {"Message": 0, "RequestInfo": 1, "NextHop": 2, "AddService": 3}
 
 class MessagePacket():
   def __init__(self, dest, payload):
@@ -86,7 +86,44 @@ class NextHopPacket():
     nextHop = packet[4:].decode()
     return cls(dest, nextHop)
 
-numToClass = {0: MessagePacket, 1: RequestInfoPacket, 2: NextHopPacket}
+class AddServicePacket():
+  def __init__(self, name, port, forwarderName):
+    self.type = typeToNum["AddService"]
+
+    self.name = name 
+    nameByteStrs = name.split(":")
+    self.nameBytes = [bytes(bytearray.fromhex(x)) for x in nameByteStrs]
+
+    self.port = port
+
+    self.forwarderName = forwarderName
+    forwarderNameByteStrs = forwarderName.split(":")
+    self.forwarderNameBytes = [bytes(bytearray.fromhex(x)) for x in forwarderNameByteStrs]
+  
+  def encode(self):
+    # Store type of packet in first byte
+    typeByte = (self.type).to_bytes(1, byteorder='big')
+    encoding = typeByte
+
+    for byte in self.nameBytes:
+      encoding += byte
+
+    encoding += (self.port).to_bytes(2, byteorder='big')
+
+    for byte in self.forwarderNameBytes:
+      encoding += byte
+
+    # Return packet bytes
+    return encoding
+
+  @classmethod
+  def decode(cls, packet):
+    name = packet[1:4].hex(":")
+    port = int.from_bytes(packet[4:6], byteorder='big')
+    forwarderName = packet[6:9].hex(":")
+    return cls(name, port, forwarderName)
+
+numToClass = {0: MessagePacket, 1: RequestInfoPacket, 2: NextHopPacket, 3: AddServicePacket}
 
 def decodePacket(packet):
   packetType = packet[0]
