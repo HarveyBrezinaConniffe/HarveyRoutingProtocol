c81fa471474a5fb7403510a511b436743e63cb68
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-12-30 15:26:14 +0000

    Forwarder buffers packets and sends RequestInfoPackets

--- Code/Forwarder.py
+++ Code/Forwarder.py
@@ -7,26 +7,26 @@ CONTROLLER_IP = "172.50.0.0"
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(("", PORT))
 
-# Map dest ID to next node IP
-nextHop = None
+myName = input("Forwarder Name> ")
 
-myIP = input("> ")
-print("Sending Hello to controller!")
-sock.sendto(Packets.HelloPacket(myIP).encode(), (CONTROLLER_IP, PORT))
+# Store packets grouped by final destination
+packetBuffer = {}
 
 def recievePacket(data, addr):
-  global nextHop
   print("PACKET INCOMING")
   packet = Packets.decodePacket(data)
+
   if packet == None:
     return
+
   if packet.type == Packets.typeToNum["Message"]:
     print("Recieving packet destined for {} with payload {}".format(packet.dest, packet.payload))
-    print("Next hop is {}".format(nextHop))
-    sock.sendto(data, (nextHop, PORT))
-  if packet.type == Packets.typeToNum["NextHop"]:
-    print("Recieving flow information")
-    nextHop = packet.hopIP
+    print("Buffering packet and asking controller for info.").
+    if packet.dest not in packetBuffer:
+      packetBuffer[packet.dest] = []
+    packetBuffer[packet.dest].append(data) 
+    reqPacket = Packets.RequestInfoPacket(packet.dest, myName)
+    sock.send(reqPacket.encode())
 
 while True:
   data, addr = sock.recvfrom(512)
