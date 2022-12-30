2fdbb3bd421b10525e3d224cf8427d8d1221f822
Harvey Brezina Conniffe <harvey@Harveys-MacBook-Pro.local>
2022-12-30 15:10:18 +0000

    Removed nextHop and Hello packet types.

--- Code/Packets.py
+++ Code/Packets.py
@@ -1,4 +1,4 @@
-typeToNum = {"Message": 0, "Hello": 1, "NextHop": 2}
+typeToNum = {"Message": 0}
 
 class MessagePacket():
 	def __init__(self, dest, payload):
@@ -25,45 +25,7 @@ class MessagePacket():
 		payload = packet[4:]
 		return cls(dest, payload)
 
-class HelloPacket():
-	def __init__(self, routerIP):
-		self.type = typeToNum["Hello"]
-		self.routerIP = routerIP
-	
-	def encode(self):
-		# Store type of packet in first byte
-		typeByte = (self.type).to_bytes(1, byteorder='big')
-		encoding = typeByte
-		encoding += str.encode(self.routerIP)
-		# Return packet bytes
-		return encoding
-
-	@classmethod
-	def decode(cls, packet):
-		# Everything after type byte is filename	
-		routerIP = packet[1:].decode()
-		return cls(routerIP)
-
-class NextHopPacket():
-	def __init__(self, hopIP):
-		self.type = typeToNum["NextHop"]
-		self.hopIP = hopIP
-	
-	def encode(self):
-		# Store type of packet in first byte
-		typeByte = (self.type).to_bytes(1, byteorder='big')
-		encoding = typeByte
-		encoding += str.encode(self.hopIP)
-		# Return packet bytes
-		return encoding
-
-	@classmethod
-	def decode(cls, packet):
-		# Everything after type byte is filename	
-		hopIP = packet[1:].decode()
-		return cls(hopIP)
-
-numToClass = {0: MessagePacket, 1: HelloPacket, 2: NextHopPacket}
+numToClass = {0: MessagePacket}
 
 def decodePacket(packet):
 	packetType = packet[0]
